4e00996ac78d519a400f73ea041f3908f0fbbef1
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2012-04-01 16:03:04 -0300

    add empty notes datasource

--- MobileAcademyDemo/NotesTableViewController.cs
+++ MobileAcademyDemo/NotesTableViewController.cs
@@ -7,6 +7,36 @@ namespace MobileAcademyDemo
 	{
 		public NotesTableViewController ()
 		{
+			Title = "My Notes";
+			TableView.DataSource = new NotesDataSource();
+		}
+	}
+	
+	public class NotesDataSource : UITableViewDataSource 
+	{
+		public override int NumberOfSections (UITableView tableView)
+		{
+			return 1;
+		}	
+		
+		public override int RowsInSection (UITableView tableView, int section)
+		{
+			return 1;
+		}
+		
+		public override UITableViewCell GetCell (UITableView tableView, MonoTouch.Foundation.NSIndexPath indexPath)
+		{
+			var noteId = "noteid";
+			var cell = tableView.DequeueReusableCell(noteId);
+				
+			if (cell == null)
+			{
+				cell = new UITableViewCell(UITableViewCellStyle.Default, noteId);
+				cell.Accessory = UITableViewCellAccessory.None;
+			}
+				
+			cell.TextLabel.Text = "Some note";
+			return cell;
 		}
 	}
 }
